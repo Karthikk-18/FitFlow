4439ef779eb3f22ea5e0db1a27317f2dde479f23
Karthik Yadav <karthikgudaru72@gmail.com>
2026-02-06 23:06:13 +0530

    Implemented Spring Security

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,9 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.dto.LoginRequestDto;
+import com.example.demo.dto.RegisterRequestDto;
 import com.example.demo.service.AuthService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
@@ -13,5 +16,19 @@ public class AuthController {
 
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDto> register(@Valid @RequestBody
+                                                    RegisterRequestDto requestDto){
+        AuthResponseDto responseDto = authService.register(requestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDto> login(@Valid @RequestBody
+                                                 LoginRequestDto loginRequestDto){
+        AuthResponseDto responseDto = authService.login(loginRequestDto);
+        return  ResponseEntity.ok(responseDto);
     }
 }
